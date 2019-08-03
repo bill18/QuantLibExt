9b689370fad8304d353a4cae3974f051ed8e9c39
bill18 <fin.hcny@gmail.com>
2019-08-03 07:29:51 -0400

    Move methods into the inner classes and add function to set loaders

--- ext/ScheduleManager.py
+++ ext/ScheduleManager.py
@@ -27,8 +27,33 @@ class ScheduleManager:
             else:
                 self.loader = loader
 
+        def setLoader(self, loader):
+            self.loader = loader
+
         def getLoader(self):
             return self.loader
+
+        def createSchedule(self, prodId):
+            data = self.loader.load(prodId)
+            # print(data)
+            schedule = qlx.Schedule(
+                data["dates"], data["calendar"], data["rolling"], data["term_rolling"], data["tenor"], None, False)
+            return schedule
+
+        def getSchedule(self, prodId):
+            if isinstance(prodId, ql.Schedule):
+                return prodId
+
+            prodId = prodId.upper()
+            if prodId in self.scheduleCache:
+                return self.scheduleCache[prodId]
+
+            schedule = self.createSchedule(prodId)
+
+            if schedule is not None:
+                self.scheduleCache[prodId] = schedule
+
+            return schedule
 
         def __str__(self):
             return repr(self) + 'Impl of ScheduleManager'
@@ -39,31 +64,15 @@ class ScheduleManager:
         if not ScheduleManager.instance:
             ScheduleManager.instance = ScheduleManager.__ScheduleManager(
                 loader)
+        elif loader is not None:
+            ScheduleManager.instance.setLoader(loader)
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
-    def createSchedule(self, prodId):
-        data = self.loader.load(prodId)
-        # print(data)
-        schedule = qlx.Schedule(
-            data["dates"], data["calendar"], data["rolling"], data["term_rolling"], data["tenor"], None, False)
-        return schedule
 
-    def getSchedule(self, prodId):
-        if isinstance(prodId, ql.Schedule):
-            return prodId
-
-        prodId = prodId.upper()
-        if prodId in self.scheduleCache:
-            return self.scheduleCache[prodId]
-
-        schedule = self.createSchedule(prodId)
-
-        if schedule is not None:
-            self.scheduleCache[prodId] = schedule
-
-        return schedule
+def setLoader(loader):
+    _ = ScheduleManager(loader)
 
 
 def getSchedule(prodId):
